b8e794529dbc0266d0d279f4e9faed9bcb84dca3
Blue Eu <eu.blue@pm.me>
2022-05-08 08:43:39 +0900

    7kyu - jaden casing strings

---
+++ 7kyu-jaden-casing-strings/README.md
@@ -0,0 +1,1 @@
+# [7 kyu - Jaden Casing Strings](https://www.codewars.com/kata/5390bac347d09b7da40006f6/train/typescript)
---
+++ 7kyu-jaden-casing-strings/jaden-casing-strings.ts
@@ -0,0 +1,3 @@
+String.prototype.toJadenCase = function () {
+    return this.split(' ').map((s) => s[0].toUpperCase() + s.slice(1)).join(' ');
+};
