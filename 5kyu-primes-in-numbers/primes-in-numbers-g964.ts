b57cdb09fe59caeb9fe5309deee05a225e25aa3d
Blue Eu <eu.blue@pm.me>
2022-05-08 11:03:01 +0900

    primes in numbers

---
+++ 5kyu-primes-in-numbers/primes-in-numbers-g964.ts
@@ -0,0 +1,19 @@
+export class G964 {
+    public static primeFactors = (n) => {
+        var i = 2, count, output = '';
+        while (n > 1) {
+            count = 0;
+            while (n % i == 0) {
+                count++;
+                n /= i;
+            }
+            switch (count) {
+                case 0: output += ""; break;
+                case 1: output += "(" + i + ")"; break;
+                default: output += "(" + i + "**" + count + ")";
+            }
+            i++;
+        }
+        return output;
+    }
+}
